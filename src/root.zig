fb116c81ac151bb5d5b7c1b14d49d1b4f5574209
Caio Silva <caio.cdcs@gmail.com>
2025-08-24 17:39:05 +0100

    create new filters module

--- src/root.zig
+++ src/root.zig
@@ -2,18 +2,15 @@ pub const BloomFilter = bloom_filter.BloomFilter;
 pub const CountingBloomFilter = counting_bloom_filter.CountingBloomFilter;
 pub const DefaultCountingBloomFilter = counting_bloom_filter.DefaultCountingBloomFilter;
 pub const ScalableBloomFilter = scalable_bloom_filter.ScalableBloomFilter;
-pub const CountingBitArray = counting_bit_array.CountingBitArray;
-pub const DefaultCountingBitArray = counting_bit_array.DefaultCountingBitArray;
 pub const QuotientFilter = quotient_filter.QuotientFilter;
 pub const CuckooFilter = cuckoo_filter.CuckooFilter;
 pub const DefaultCuckooFilter = cuckoo_filter.DefaultCuckooFilter;
 
-const bloom_filter = @import("bloom_filter.zig");
-const counting_bloom_filter = @import("counting_bloom_filter.zig");
-const scalable_bloom_filter = @import("scalable_bloom_filter.zig");
-const counting_bit_array = @import("counting_bit_array.zig");
-const quotient_filter = @import("quotient_filter.zig");
-const cuckoo_filter = @import("cuckoo_filter.zig");
+const bloom_filter = @import("filters/bloom_filter.zig");
+const counting_bloom_filter = @import("filters/counting_bloom_filter.zig");
+const scalable_bloom_filter = @import("filters/scalable_bloom_filter.zig");
+const quotient_filter = @import("filters/quotient_filter.zig");
+const cuckoo_filter = @import("filters/cuckoo_filter.zig");
 
 const std = @import("std");
 test {
